0d724074204f364a93dfcc08a6888ff82612f92a
frifri84250 <frigel84@hotmail.fr>
2025-08-25 01:06:56 +0200

    Add .next/static/chunks/src_app_dashboard_page_9b5cffc0.js 1756076815948

---
+++ .next/static/chunks/src_app_dashboard_page_9b5cffc0.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_skinview3d_libs_skinview3d_bd1ca6e4.js",
+  "static/chunks/_3d40c044._.js"
+],
+    source: "dynamic"
+});
